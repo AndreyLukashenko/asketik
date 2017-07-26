1cf72d73d8a6328be67a9deabd565603f5e7d3f3
AndreyLukashenko <LukashenkoAndrey@gmail.com>
2017-07-26 22:31:36 +0300

    Add About component

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const About = () => {
+    const style = {
+        margin: 0,
+        color: '#545454',
+        fontSize: '12px',
+    };
+
+    return <p style={style}>Asketik – minimum for maximum</p>
+}
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,8 +6,8 @@ import './Menu.scss';
 export const Menu = () => (
     <nav>
         <ul className="menu">
-            <li><Link to="/about">about</Link></li>
-            <li><Link to="/contact">contact</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
         </ul>
     </nav>
 )
